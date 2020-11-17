8ea53bd68cfa6fdfe27565b4aefcabd6d5e6bfe9
LaurieReinette <laurie97275@hotmail.fr>
2020-11-17 16:33:05 +0100

    addition of id for routes

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/api")
+ */
+class ApiController extends AbstractController
+{
+     /**
+     * @Route("/navpages", name="navpages", methods="GET")
+     */
+    public function navpages()
+    {
+        $pages = 
+        [
+            [
+                "route" => "",
+                "label" => "Accueil",
+                "id" => 1
+            ],
+            [
+                "route" => "mon-compte",
+                "label" => "Mon Compte",
+                "id" => 2
+            ],
+            [
+                "route" => "a-propos",
+                "label" => "A propos",
+                "id" => 3
+            ]
+            
+        ];
+        return $this->json($pages, $status = 200, $pages, $context = []);
+    }
+}
