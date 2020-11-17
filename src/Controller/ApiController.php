a3442d9ca536864a4a4c13fd6814958abdf85c05
LaurieReinette <laurie97275@hotmail.fr>
2020-11-17 16:39:26 +0100

    addition of id for routes

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -21,26 +21,17 @@ class ApiController extends AbstractController
             [
                 "route" => "",
                 "label" => "Accueil",
-<<<<<<< HEAD
                 "id" => 1
-=======
->>>>>>> a0faf4e48419dd4c699bdb7e940bd5f1a49c21f6
             ],
             [
                 "route" => "mon-compte",
                 "label" => "Mon Compte",
-<<<<<<< HEAD
                 "id" => 2
-=======
->>>>>>> a0faf4e48419dd4c699bdb7e940bd5f1a49c21f6
             ],
             [
                 "route" => "a-propos",
                 "label" => "A propos",
-<<<<<<< HEAD
                 "id" => 3
-=======
->>>>>>> a0faf4e48419dd4c699bdb7e940bd5f1a49c21f6
             ]
             
         ];
