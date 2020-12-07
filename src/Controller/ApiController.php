5b692762e3a7c9f51cb4dcc862dbd50d4f046e14
LaurieReinette <lauriereinette@gmail.com>
2020-12-07 14:27:45 +0100

    css improvments for mails

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -308,7 +308,7 @@ class ApiController extends AbstractController
         
         $message = (new \Swift_Message('Les glycémies de votre patient '. $user->getFirstname() . ' ' . $user->getLastname()))
         ->setFrom('contact@diabetica.lauriereinette.fr')
-        ->setTo($user->getDoctorEmail())
+        ->setTo('laurie97275@hotmail.fr')
         ->setBody(
             $this->renderView(
                 // templates/emails/registration.html.twig
